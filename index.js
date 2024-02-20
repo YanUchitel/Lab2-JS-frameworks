9b81198952cf0fb1090a9ce075d2f772b09f679d
hansnery <hansnery@gmail.com>
2024-02-20 09:34:34 -0500

    2 more functions

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const Module = require('./module');
 Module.printMsg("test text")
 
 console.log(Module.InchesToCentimeters(2.78))
+
+console.log(Module.FahrenheitToCelsius(30))
+
+console.log(Module.calculateFibonacci(10))
--- module.js
+++ module.js
@@ -9,11 +9,16 @@ exports.InchesToCentimeters = (inches) => {
 
 // do any math operation against a numeric value(s), it can accept one, or more than one 
 // value (argument) and then return the result of applying a math formula 
-exports.f3 = () => {
-
+exports.FahrenheitToCelsius = (temperature) => {
+    // returns converted value in Celsius
+    return ((temperature - 32) * 5/9)
 }
 
 // any advanced function of your choice
-exports.f4 = () => {
-
+exports.calculateFibonacci = (n) => {
+    const sequence = [0, 1];
+    for (let i = 2; i < n; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+    return sequence;
 }
